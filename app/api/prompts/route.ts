2d61493c6d1e2ce43e075b4aeaeb26bce3582295
Oli Young <oliyoung@gmail.com>
2024-11-06 13:13:00 +1030

    Adds PUT route to prompts to update prompts and clear responses

--- app/api/prompts/route.ts
+++ app/api/prompts/route.ts
@@ -2,7 +2,7 @@
 
 import db from "@/app/db";
 import { eq } from "drizzle-orm";
-import { prompts } from "@/app/db/schema";
+import { prompts, responses } from "@/app/db/schema";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -28,6 +28,26 @@ export async function POST(request: NextRequest) {
     try {
         const insertResponse = await db.insert(prompts).values({ body }).returning();
         return new NextResponse(JSON.stringify(insertResponse), { status: 201, });
+    } catch (error) {
+        return new NextResponse(JSON.stringify({ error }), { status: 400 });
+    }
+}
+
+export async function PUT(request: NextRequest) {
+    const promptId = Number(request.nextUrl.searchParams.get("promptId"));
+
+    const { body } = await request.json();
+    if (!body) {
+        return new NextResponse(
+            JSON.stringify({ error: "No Prompt Provided" }),
+            { status: 400 }
+        );
+    }
+
+    try {
+        const updatePrompt = await db.update(prompts).set({ body }).returning();
+        const deleteResponses = await db.delete(responses).where(eq(responses.promptId, promptId));
+        return new NextResponse(JSON.stringify(updatePrompt), { status: 202 });
     } catch (error) {
         return new NextResponse(JSON.stringify({ error }), { status: 400 });
     }
