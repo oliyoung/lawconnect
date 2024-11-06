351a8cf49647c5aa7376763ae2ebc4b589f1fe37
Oli Young <oliyoung@gmail.com>
2024-11-06 13:02:31 +1030

    Adds simple CRUD (non API backed) GET & POST routes for Prompts

--- app/api/prompts/route.ts
+++ app/api/prompts/route.ts
@@ -1,19 +1,34 @@
+"use server";
+
+import db from "@/app/db";
+import { eq } from "drizzle-orm";
+import { prompts } from "@/app/db/schema";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-    const json = {}
+export async function GET(request: NextRequest) {
+    const promptId = request.nextUrl.searchParams.get("promptId");
 
-    return NextResponse.json(json);
+    try {
+        const selectResponse = await db.select().from(prompts).where(eq(prompts.id, Number(promptId)));
+        return new NextResponse(JSON.stringify(selectResponse));
+    } catch (error) {
+        return new NextResponse(JSON.stringify({ error }), { status: 400 });
+    }
 }
 
 export async function POST(request: NextRequest) {
-    const { promptBody } = await request.json();
-    if (!promptBody) {
+    const { body } = await request.json();
+    if (!body) {
         return new NextResponse(
             JSON.stringify({ error: "No Prompt Provided" }),
             { status: 400 }
         );
     }
 
-    return new NextResponse(JSON.stringify({}), { status: 201, });
+    try {
+        const insertResponse = await db.insert(prompts).values({ body }).returning();
+        return new NextResponse(JSON.stringify(insertResponse), { status: 201, });
+    } catch (error) {
+        return new NextResponse(JSON.stringify({ error }), { status: 400 });
+    }
 }
